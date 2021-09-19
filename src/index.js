82cc3ce8c073262574c0eafe1e0438df7c4dfc49
LeilaSchooley <vortex962@zoho.com>
2021-09-19 16:08:37 +0100

    Separated different responsibilities into functions, added separate file for modules

--- src/index.js
+++ src/index.js
@@ -1,85 +1,60 @@
 import component from "./createElement.js"
+import {createNewTodo, allProjects} from "./createNewTodo.js"
 import './style.css';
 
 
-let defaultProject = []
 
-let createToDoList = (title, description, dueDate, priority) => {
+
+
+function renderHomepage(){
+
     
-    return {title, description, dueDate, priority }
+    document.body.appendChild(component("<h1 class='center'>To-Do-List</h1>"))
+    
+    
+    document.body.appendChild(component("<button id='new-project'>Create New Project</button>"))
+    
+    document.body.appendChild(component("<button id='projects' class='project-buttons'>Default</button>"))
+    
+
+    document.body.appendChild(component("<button id='new-todo'>Create New Todo</button>"))
+
+
+    let createToDoListButtonClick = document.getElementById("new-todo")
+    createToDoListButtonClick.addEventListener("click",() => {
+        createNewTodo()
+    })
+    
 }
+renderHomepage()
+
+    
+
+function createNewProject(){
+    let newProjectsButton = document.getElementById("new-project")
 
 
-let mainContent = document.createElement("div")
-
-document.body.appendChild(component("<h1>To-Do-List</h1>"))
-
-document.body.appendChild(component("<button id='projects'>Default</button>"))
-
-
-document.body.appendChild(component("<button id='new-project'>Create New Project</button>"))
-
-let newProjectsButton = document.getElementById("new-project")
-
-newProjectsButton.addEventListener("click", () => {
-    document.body.appendChild(component(`<label>Name of Project:<input id='project-name'></input></label>
-    <button id='submit-project'>Submit New Project</button></label>`))
-
-
-    document.body.appendChild(component(""))
-
-})
-
-
-
-
-let allProjectsButton = document.getElementById("projects")
-
-
-
-
-document.body.appendChild(mainContent)
-
-let headerTag = document.querySelector("h1")
-
-headerTag.classList.add("center")
-
-
-let createToDoListButton = document.createElement("button")
-
-createToDoListButton.textContent="Create New Todo"
-
-createToDoListButton.id="new-todo"
-mainContent.appendChild(createToDoListButton)
-
-let createToDoListButtonClick = document.getElementById("new-todo")
-createToDoListButtonClick.addEventListener("click",() => {
-    addNewTodo()
-})
-
-
-function addNewTodo(){
-    document.body.appendChild(component("<label>Title<input id='title'></input></label>"))
-    document.body.appendChild(component("<label>Description<input id='description'></input></label>"))
-    document.body.appendChild(component("<label>dueDate<input id='dueDate'></input></label>"))
-    document.body.appendChild(component("<label>Priority<input id='priority'></input></label>"))
-    document.body.appendChild(component("<button id='submit-todo'>Submit</button>"))
-
-    let todoSubmit = document.getElementById("submit-todo")
-
-    todoSubmit.addEventListener("click",() => {
-        let title = document.getElementById("title").value
-        let description = document.getElementById("description").value
-        let dueDate = document.getElementById("dueDate").value
-        let priority = document.getElementById("priority").value
-
-        let newTodoList = createToDoList (title, description, dueDate, priority)
-        defaultProject.push(newTodoList)
-        
-
+    newProjectsButton.addEventListener("click", () => {
+        document.body.appendChild(component(`<label>Name of Project:<input id='project-name'></input></label>
+        <button id='submit-project'>Submit New Project</button></label>`))
+        submitNewProject()
+    
     })
 
 
+}
+
+function submitNewProject(){
+    let submitProject = document.getElementById("submit-project")
+
+    submitProject.addEventListener("click",() => {
+        let projectName = document.getElementById("project-name").value
+        allProjects[`${projectName}`] = []
+        document.body.appendChild(component(`<button id='${projectName}' class='project-buttons'>${projectName}</button>`))
+        activeProjectName()
+                                
+    })
 
 }
 
+createNewProject()
